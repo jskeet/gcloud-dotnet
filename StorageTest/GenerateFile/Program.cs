b496255bfc04abde5faca4d57d90ccccca2247d1
Jon Skeet <jonskeet@google.com>
2021-07-14 10:17:09 +0100

    Storage tests

---
+++ StorageTest/GenerateFile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace GenerateFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = args[0];
+            long size = long.Parse(args[1]);
+
+            byte[] buffer = new byte[65536];
+            var random = new Random();
+            using var stream = File.Create(name);
+            while (size > 0)
+            {
+                int chunk = (int) Math.Min(buffer.Length, size);
+                random.NextBytes(buffer);
+                stream.Write(buffer, 0, chunk);
+                size -= chunk;
+            }
+        }
+    }
+}
---
+++ StorageTest/UploadFile/Program.cs
@@ -0,0 +1,24 @@
+﻿using Google.Cloud.Storage.V1;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace UploadFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = StorageClient.Create();
+            string bucket = args[0];
+            string obj = args[1];
+            string file = args[2];
+
+            var stream = File.OpenRead(file);
+            var stopwatch = Stopwatch.StartNew();
+            client.UploadObject(bucket, obj, "application/binary", stream);
+            stopwatch.Stop();
+            Console.WriteLine($"Upload completed in {stopwatch.Elapsed.TotalSeconds}s");
+        }
+    }
+}
